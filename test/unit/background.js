81be1b39cd9366ecb937d8e1bee1b14da561c7ff
André Luiz Cardoso <andrecardoso@gmail.com>
2018-02-21 17:42:23 -0300

    Add tests

---
+++ test/unit/background.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const chrome = require('sinon-chrome/apps');
+
+describe('background script', () => {
+
+  before(() => {
+    global.chrome = chrome;
+    global.screen = {availWidth: 1000, availHeight: 1000};
+  });
+
+  it('should create player window when app is launched', () => {
+    require('../../src/background'); // eslint-disable-line global-require
+
+    assert(chrome.app.runtime.onLaunched.addListener.calledOnce, 'chrome.app.runtime.onLaunched.addListener should have been called');
+
+    chrome.app.runtime.onLaunched.dispatch({})
+
+    const expectedWindowOptions = {
+      id: 'player',
+      outerBounds: {
+        width: 600,
+        height: 500,
+        left: 200,
+        top: 250
+      }
+    };
+
+    assert(chrome.app.window.create.calledWith('player.html', expectedWindowOptions), 'chrome.app.window.create should have been called');
+  });
+
+  after(() => {
+    chrome.flush();
+    Reflect.deleteProperty(global, 'chrome');
+    Reflect.deleteProperty(global, 'screen');
+  });
+
+});
