8bacefdfce80d11e6b75bd24d46f0ff39daafe06
André Luiz Cardoso <andrecardoso@gmail.com>
2018-02-23 13:33:36 -0300

    Validate display ID when "Continue" is clicked

--- src/player.js
+++ src/player.js
@@ -1,5 +1,26 @@
+const validator = require('./display-id-validator');
+
 function init() {
-  console.log('player');
+  const form = document.querySelector('form');
+  form.onsubmit = () => {
+    const displayId = form.querySelector('input').value.toUpperCase();
+
+    const errorMessage = document.getElementById('errorMessage');
+    if (!displayId) {
+      errorMessage.innerHTML = '<span>Please enter a Display ID</span>';
+      errorMessage.style.display = 'block';
+      return;
+    }
+
+    validator.validateDisplayId(displayId)
+      .then(() => {
+        console.log('Launch viewer');
+      })
+      .catch(() => {
+        errorMessage.innerHTML = `<span>The Display ID <b>${displayId}</b> is not valid or does not exist`;
+        errorMessage.style.display = 'block';
+      });
+  };
 }
 
 document.addEventListener("DOMContentLoaded", init);
