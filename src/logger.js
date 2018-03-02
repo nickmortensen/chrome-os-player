8d3256670ce79e1053b2dd5df3fde897db8b0023
André Luiz Cardoso <andrecardoso@gmail.com>
2018-03-02 15:36:15 -0300

    Add method to log client info into Player_Data.configuration

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,30 @@
 const bq = require('./bq-client');
 const systemInfo = require('./system-info');
 
+function logClientInfo(viewerConfig, nowDate = new Date()) {
+  return Promise.all([systemInfo.getMachineId(), systemInfo.getDisplayId(), systemInfo.getOS(), systemInfo.getIpAddress()])
+    .then(values => {
+      const [machineId, displayId, os, ip] = values;
+      const playerData = {
+        machine_id: machineId,
+        display_id: displayId,
+        os_description: os,
+        player_name: systemInfo.getPlayerName(),
+        player_version: systemInfo.getPlayerVersion(),
+        browser_name: 'Chrome',
+        browser_version: systemInfo.getChromeVersion(),
+        local_ip: ip,
+        viewer_version: viewerConfig.viewerVersion,
+        width: viewerConfig.width,
+        height: viewerConfig.height,
+        ts: nowDate.toISOString()
+      };
+
+      return bq.insert(playerData, 'Player_Data', 'configuration');
+    })
+    .catch(console.error);
+}
+
 function log(event, details) {
   console.log(event, details);
 
@@ -23,5 +47,6 @@ function log(event, details) {
 }
 
 module.exports = {
-  log
+  log,
+  logClientInfo
 }
--- test/unit/logger.js
+++ test/unit/logger.js
@@ -27,6 +27,41 @@ describe('Logger', () => {
       });
   });
 
+  it('should log client info to big query', () => {
+    sandbox.stub(bq, 'insert');
+
+    sandbox.stub(systemInfo, 'getMachineId').returns('machineId');
+    sandbox.stub(systemInfo, 'getDisplayId').returns('displayId');
+    sandbox.stub(systemInfo, 'getOS').returns('mac/x86-64');
+    sandbox.stub(systemInfo, 'getIpAddress').returns('192.168.0.1');
+    sandbox.stub(systemInfo, 'getPlayerName').returns('(Beta) RisePlayerChromeOS');
+    sandbox.stub(systemInfo, 'getPlayerVersion').returns('beta_0.0.0.0');
+    sandbox.stub(systemInfo, 'getChromeVersion').returns('64.0.3282.186');
+
+    const viewerConfig = {width: 1000, height: 1000, viewerVersion: 'viewerVersion'};
+    const nowDate = new Date();
+
+    const expectedPlayerData = {
+      machine_id: 'machineId',
+      display_id: 'displayId',
+      os_description: 'mac/x86-64',
+      player_name: '(Beta) RisePlayerChromeOS',
+      player_version: 'beta_0.0.0.0',
+      browser_name: 'Chrome',
+      browser_version: '64.0.3282.186',
+      local_ip: '192.168.0.1',
+      viewer_version: viewerConfig.viewerVersion,
+      width: viewerConfig.width,
+      height: viewerConfig.height,
+      ts: nowDate.toISOString()
+    };
+
+    return logger.logClientInfo(viewerConfig, nowDate)
+      .then(() => {
+        sinon.assert.calledWith(bq.insert, expectedPlayerData, 'Player_Data', 'configuration');
+      });
+  });
+
   after(() => {
     chrome.flush();
     Reflect.deleteProperty(global, 'chrome');
