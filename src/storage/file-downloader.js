3c19aa197423df44261561f8be271b645f34af63
André Luiz Cardoso <andrecardoso@gmail.com>
2018-03-19 18:09:58 -0300

    Add file downloader object

---
+++ src/storage/file-downloader.js
@@ -0,0 +1,65 @@
+const fileSystem = require("./file-system");
+const urlProvider = require("./url-provider");
+
+/**
+ * Downloads a file entry and saves it to disk
+ * @param {object} entry with filePath, version and token properties
+ * @returns {Promise.<FileEntry>}
+ */
+function download(entry) {
+  const {filePath, version, token} = entry;
+
+  return fileSystem.checkAvailableDiskSpace()
+    .then((availableSpace) => {
+      if (!availableSpace) {
+        return Promise.reject(Error('Insufficient disk space'));
+      }
+
+      return urlProvider.getUrl(token);
+    })
+    .then((signedUrl) => {
+      if (!signedUrl) {
+        return Promise.reject(Error('No signed URL'));
+      }
+
+      return requestFile(signedUrl);
+    })
+    .then(response => validateResponse(response))
+    .then(response => {
+      const fileName = `${filePath}${version}`;
+      const dirName = 'downloads';
+      return fileSystem.writeFileToDirectory(fileName, response.body, dirName);
+    });
+}
+
+function requestFile(signedUrl, retries = 2) { // eslint-disable-line no-magic-numbers
+  return fetch(signedUrl).catch(error => {
+    if (retries <= 0) {
+      return Promise.reject(error);
+    }
+
+    return requestFile(signedUrl, retries - 1);
+  });
+}
+
+function validateResponse(response) {
+  if (!response.ok) {
+    return Promise.reject(Error(`Invalid response with status code ${response.status}`));
+  }
+
+  const contentLength = response.headers.get('Content-Length');
+  const fileSize = Number.parseInt(contentLength, 10) || 0;
+
+  return fileSystem.checkAvailableDiskSpace(fileSize)
+    .then((availableSpace) => {
+      if (!availableSpace) {
+        return Promise.reject(Error('Insufficient disk space'));
+      }
+
+      return response;
+    });
+}
+
+module.exports = {
+  download
+}
---
+++ test/unit/storage/file-downloader.js
@@ -0,0 +1,126 @@
+const assert = require('assert');
+const sinon = require('sinon');
+
+const fileSystem = require('../../../src/storage/file-system');
+const urlProvider = require('../../../src/storage/url-provider');
+
+const fileDownloader = require('../../../src/storage/file-downloader');
+
+const sandbox = sinon.createSandbox();
+const fetch = sandbox.stub();
+
+const filePath = 'test-path';
+const version = 'version';
+const token = {
+  hash: 'abc123',
+  data: {
+    displayId: 'test-display-id',
+    timestamp: Date.now(),
+    filePath: 'test-path'
+  }
+};
+
+describe('File Downloader', () => {
+
+  before(() => global.fetch = fetch);
+
+  after(() => Reflect.deleteProperty(global, 'fetch'));
+
+  afterEach(() => sandbox.restore());
+  afterEach(() => fetch.reset());
+
+  it('should download successfully', () => {
+    const expectedFileEntry = {};
+
+    sandbox.stub(fileSystem, 'writeFileToDirectory').resolves(expectedFileEntry);
+    sandbox.stub(fileSystem, 'checkAvailableDiskSpace').resolves(true);
+    sandbox.stub(urlProvider, 'getUrl').resolves('http://risevision.com/test/file');
+    fetch.resolves({ok: true, status: 200, headers: {get() {}}});
+
+    const entry = {filePath, version, token};
+
+    return fileDownloader.download(entry)
+      .then(fileEntry => {
+        assert.equal(fileEntry, expectedFileEntry);
+      });
+  });
+
+  it('should reject and not get signed url when no available space"', () => {
+    sandbox.stub(fileSystem, 'checkAvailableDiskSpace').resolves(false);
+    sandbox.stub(urlProvider, 'getUrl').resolves('http://risevision.com/test/file');
+
+    const entry = {filePath, version, token};
+
+    return fileDownloader.download(entry)
+      .then(assert.fail)
+      .catch(error => {
+        assert.equal(error.message, 'Insufficient disk space');
+        sinon.assert.notCalled(urlProvider.getUrl);
+      });
+  });
+
+  it('should reject and not request file when requesting signed url fails', () => {
+    sandbox.stub(fileSystem, 'checkAvailableDiskSpace').resolves(true);
+    sandbox.stub(urlProvider, 'getUrl').rejects(Error('Invalid response with status code 500'));
+
+    const entry = {filePath, version, token};
+
+    return fileDownloader.download(entry)
+      .then(assert.fail)
+      .catch(error => {
+        assert.equal(error.message, 'Invalid response with status code 500');
+        sinon.assert.notCalled(fetch);
+      });
+  });
+
+  it('should reject and not write file when file request returns invalid response', () => {
+    sandbox.stub(fileSystem, 'writeFileToDirectory').resolves({});
+    sandbox.stub(fileSystem, 'checkAvailableDiskSpace').resolves(true);
+    sandbox.stub(urlProvider, 'getUrl').resolves('http://risevision.com/test/file');
+    fetch.resolves({ok: false, status: 500, headers: {get() {}}});
+
+    const entry = {filePath, version, token};
+
+    return fileDownloader.download(entry)
+      .then(assert.fail)
+      .catch(error => {
+        assert.equal(error.message, 'Invalid response with status code 500');
+        sinon.assert.notCalled(fileSystem.writeFileToDirectory);
+      });
+  });
+
+  it('should reject and not write file when file request rejects', () => {
+    sandbox.stub(fileSystem, 'writeFileToDirectory').resolves({});
+    sandbox.stub(fileSystem, 'checkAvailableDiskSpace').resolves(true);
+    sandbox.stub(urlProvider, 'getUrl').resolves('http://risevision.com/test/file');
+    fetch.rejects(Error('Testing'));
+
+    const entry = {filePath, version, token};
+
+    return fileDownloader.download(entry)
+      .then(assert.fail)
+      .catch(error => {
+        assert.equal(error.message, 'Testing');
+        sinon.assert.notCalled(fileSystem.writeFileToDirectory);
+      });
+  });
+
+  it('should reject and not write file when file request lenght exceeds available space', () => {
+    sandbox.stub(fileSystem, 'writeFileToDirectory').resolves({});
+    const checkAvailableDiskSpace = sandbox.stub(fileSystem, 'checkAvailableDiskSpace');
+    checkAvailableDiskSpace.onFirstCall().resolves(true);
+    checkAvailableDiskSpace.onSecondCall().resolves(false);
+    sandbox.stub(urlProvider, 'getUrl').resolves('http://risevision.com/test/file');
+    fetch.resolves({ok: true, status: 200, headers: {get() {return "100000000";}}});
+
+    const entry = {filePath, version, token};
+
+    return fileDownloader.download(entry)
+      .then(assert.fail)
+      .catch(error => {
+        assert.equal(error.message, 'Insufficient disk space');
+        sinon.assert.notCalled(fileSystem.writeFileToDirectory);
+      });
+  });
+
+});
