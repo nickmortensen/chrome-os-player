7c0a14f3ed04203cf77182fa6527f6a09166c681
André Luiz Cardoso <andrecardoso@gmail.com>
2018-03-20 20:14:34 -0300

    Add rewrites on content to use the new widgets domain

--- src/content-loader.js
+++ src/content-loader.js
@@ -9,6 +9,12 @@ function fetchContent() {
     const bucketName = 'risevision-display-notifications';
     const filePath = `${displayId}/content.json`;
     return gcsClient.fetchJson(bucketName, filePath);
+  })
+  .then((contentData) => {
+    const regex = new RegExp('http(?:s?)://s3.amazonaws.com/widget-(image|video)', 'g');
+    const rewriteUrl = (match, widgetName) => `http://widgets.risevision.com/widget-${widgetName}`; // eslint-disable-line func-style
+    contentData.content.presentations.forEach((presentation) => presentation.layout = presentation.layout.replace(regex, rewriteUrl));
+    return contentData;
   });
 }
 
