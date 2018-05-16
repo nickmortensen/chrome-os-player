2f683b149a01982a6565f17e07679c42677327a2
André Luiz Cardoso <andrecardoso@gmail.com>
2018-05-16 18:56:20 -0300

    Add cache clean up to local storage

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -2,12 +2,14 @@ const messaging = require('./messaging/messaging');
 const downloadQueue = require('./download-queue');
 const watchlist = require('./messaging/watch/watchlist');
 const db = require('./db');
+const fileSystem = require('./file-system');
 
 function init() {
   messaging.init();
   downloadQueue.checkStaleFiles();
   watchlist.requestWatchlistCompare();
   db.start();
+  fileSystem.clearLeastRecentlyUsedFiles('cache');
 }
 
 module.exports = {
