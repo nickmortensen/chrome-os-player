089a270f9dbacbc272e205989e2195889719c170
André Luiz Cardoso <andrecardoso@gmail.com>
2018-07-23 19:06:18 -0300

    Check if file is readable before sending current file update messages

--- src/storage/file-system.js
+++ src/storage/file-system.js
@@ -107,6 +107,15 @@ function readCachedFileAsObject(fileHash) {
 }
 
 /**
+ * @param {string} filePath
+ * @param {string} version
+ * @returns {Promise.<File>}
+ */
+function readCachedFile(filePath, version) {
+  return util.sha1(`${filePath}${version}`).then(fileName => readFile(fileName, 'cache'));
+}
+
+/**
  * @param {File} file
  * @returns {Array.<File>}
  */
@@ -258,6 +267,7 @@ function processChunkedContents(contentStream, fileWriter) {
 
 module.exports = {
   readCachedFileAsObject,
+  readCachedFile,
   createDirectory,
   getAvailableSpace,
   checkAvailableDiskSpace,
