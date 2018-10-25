7b6e4a4ee8aedb1fe0cce752ff1b90bef8a698c6
André Luiz Cardoso <andrecardoso@gmail.com>
2018-10-25 16:38:59 -0300

    Improve content fetch retry
      - Increase retry number and timeout
      - Reboot display when retry exhausted

--- src/gcs-client.js
+++ src/gcs-client.js
@@ -3,7 +3,9 @@ const serviceUrl = 'https://www.googleapis.com/storage/v1/b/BUCKETNAME/o/FILEPAT
 
 function fetchJson(bucketName, filePath) {
   const url = serviceUrl.replace("BUCKETNAME", bucketName).replace("FILEPATH", encodeURIComponent(filePath));
-  return util.fetchWithRetry(`${url}?alt=media&ifGenerationNotMatch=-1`).then(response => response.json());
+  const retries = 5;
+  const timeout = 10000;
+  return util.fetchWithRetry(`${url}?alt=media&ifGenerationNotMatch=-1`, {}, retries, timeout).then(response => response.json());
 }
 
 module.exports = {
